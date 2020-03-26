ff4b0ae149d1ff5de7fb9da39ae8fde7e08a8346
Heather <heather.miller114@gmail.com>
2020-03-26 17:01:44 -0400

    adds event listener to try to fetch notes, currently does not work

--- events_frontend/src/index.js
+++ events_frontend/src/index.js
@@ -1,3 +1,8 @@
+const endPoint = 'http://localhost:3000/api/v1/events'
+
 document.addEventListener('DOMContentLoaded', () => {
-    alert('LOADED')
+    const endPoint = 'http://localhost:3000/api/v1/events';
+    fetch(endPoint)
+        .then(res => res.json())
+        .then(json => console.log(json));
 });
