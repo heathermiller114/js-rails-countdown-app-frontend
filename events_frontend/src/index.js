a5c04381d6fefa31b3c947868846c2547d005bf4
Heather <heather.miller114@gmail.com>
2020-03-31 15:11:35 -0400

    adds event name to the DOM with edit button

--- events_frontend/src/index.js
+++ events_frontend/src/index.js
@@ -3,6 +3,17 @@ const endPoint = 'http://localhost:3000/api/v1/events'
 document.addEventListener('DOMContentLoaded', () => {
     const endPoint = 'http://localhost:3000/api/v1/events';
     fetch(endPoint)
-        .then(res => res.json())
-        .then(json => console.log(json));
-});
+      .then(res => res.json())
+      .then(json => 
+        json.forEach(event => {
+            const markup  = `
+            <li>
+                <h3>${event.name}
+                <button>edit</button>
+                </h3>
+            </li>`
+
+            document.querySelector('#events-list').innerHTML += markup;
+            })
+        );
+  });
