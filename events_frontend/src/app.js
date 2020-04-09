5fa3b82ddce362074aefffee9f29b6fbfd5e3048
Heather <heather.miller114@gmail.com>
2020-04-09 13:21:01 -0400

    changes getElementById back to querySelector in app.js -- forgot pound sign

--- events_frontend/src/app.js
+++ events_frontend/src/app.js
@@ -1,6 +1,6 @@
 class App {
     attachEventListeners() {
-        document.getElementById('events-list').addEventListener('click', e => {
+        document.querySelector('#events-list').addEventListener('click', e => {
             const id = parseInt(e.target.dataset.id)
             console.log(id)
             const event = Event.findById(id)
--- events_frontend/src/index.js
+++ events_frontend/src/index.js
@@ -2,18 +2,18 @@ const endPoint = 'http://localhost:3000/api/v1/events'
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new App();
-    console.log(app)
+    // console.log(app)
     app.attachEventListeners();
 
     const endPoint = 'http://localhost:3000/api/v1/events';
     fetch(endPoint)
       .then(res => res.json())
       .then(json => {
-        console.log(json)
+        // console.log(json)
         json.forEach(function(event) {
-            console.log(event)
+            // console.log(event)
             let newEvent = new Event(event);
-            console.log(newEvent)
+            // console.log(newEvent)
             document.querySelector('#events-list').innerHTML += newEvent.renderEventItem();
             });
       });
