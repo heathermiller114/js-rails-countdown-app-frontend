9b25722024e1d55d1208d24db77e04a4a3856858
Heather <heather.miller114@gmail.com>
2020-04-10 17:18:31 -0400

    creates variables pulled from data when click submit button in update

--- events_frontend/src/app.js
+++ events_frontend/src/app.js
@@ -2,10 +2,23 @@ class App {
     attachEventListeners() {
         document.querySelector('#events-list').addEventListener('click', e => {
             const id = parseInt(e.target.dataset.id)
-            console.log(id)
+            // console.log(id)
             const event = Event.findById(id)
-            console.log(event)
+            // console.log(event)
             document.querySelector('#update').innerHTML = event.renderUpdateForm()
         })
+        
+        document.querySelector('#update').addEventListener('submit', e => {
+            e.preventDefault();
+            // console.log(e)
+            const id = parseInt(e.target.dataset.id)
+            const event = Event.findById(id)
+            // console.log(event)
+            const name = e.target.querySelector('input').value
+            // console.log(name)
+            const date = e.target.getElementsByTagName('p')[1].querySelector('input').value
+            console.log(date)
+        })
     }
+
 }
